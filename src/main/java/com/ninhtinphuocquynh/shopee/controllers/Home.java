cb6439b6bc791e92eb0e303db4406b921b130426
Nguyễn Ninh <nguyenvanninh2004pt@gmail.com>
2023-10-25 09:52:59 +0700

    25/10/2023

--- src/main/java/com/ninhtinphuocquynh/shopee/controllers/Home.java
+++ src/main/java/com/ninhtinphuocquynh/shopee/controllers/Home.java
@@ -4,9 +4,9 @@ import com.ninhtinphuocquynh.shopee.model.SliderBanner;
 import com.ninhtinphuocquynh.shopee.service.CategoryService;
 import com.ninhtinphuocquynh.shopee.service.SliderBannerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
--- src/main/java/com/ninhtinphuocquynh/shopee/repository/CategoryRepository.java
+++ src/main/java/com/ninhtinphuocquynh/shopee/repository/CategoryRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface CategoryRepository extends CrudRepository<Category, Integer> {
     @Override
     List<Category> findAll();
+
 }
